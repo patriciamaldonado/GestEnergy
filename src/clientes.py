aa5a178db6b818bc69800b6c1f5fe9b1841e6877
Patricia <patriimaldonado@gmail.com>
2019-10-10 01:30:54 +0200

    añadidos archivos necesarios para tests closed #3

--- src/clientes.py
+++ src/clientes.py
@@ -1,11 +1,7 @@
-import unittest
 import json
 
 
-
 class Clientes:
-
-
 
     def __init__(self):
         with open('data/datosClientes.json', 'r') as f:
@@ -16,13 +12,66 @@ class Clientes:
             listado = self.clientes = json.load(f)
         return listado
 
-
     def busquedaPorNombre(self,nombre):
-        n = []
-        if type(nombre) == int:
-            return False
-        for i in self.clientes:
-            if i["nombre"] == nombre:
-                n.append(i)
+            n = []
+            if type(nombre) == int:
+                n = False
                 return n
-        return False
+            for cliente in self.clientes:
+                if cliente["Nombre"] in nombre:
+                        n.append(cliente)
+            if not n:
+                n = None
+                return n
+            return n
+
+    def busquedaPorDNI(self,DNI):
+            n = []
+            for cliente in self.clientes:
+                if cliente["DNI"] in DNI:
+                        n.append(cliente)
+            if not n:
+                n = None
+                return n
+            return n
+    def busquedaPorProvincia(self,provincia):
+            n = []
+            if type(provincia) == int:
+                n = False
+                return n
+            for cliente in self.clientes:
+                if cliente["Provincia"] in provincia:
+                        n.append(cliente)
+            if not n:
+                n = None
+                return n
+            return n
+
+    def busquedaPorEstado(self,estado):
+            n = []
+            if type(estado) == int:
+                n = False
+                return n
+            for cliente in self.clientes:
+                if cliente["Estado"] in estado:
+                        n.append(cliente)
+            if not n:
+                n = None
+                return n
+            return n
+
+    def busquedaPorRobinson(self,nombre):
+            n = []
+            if type(nombre) == int:
+                n = False
+                return n
+            for i in self.clientes:
+                if i["Nombre"] == nombre:
+                        if i["Robinson"] == "no":
+                            n.append(i)
+                            return n
+                        else:
+                            n = None
+                            return "No puede ofrecer ofertas a este cliente"
+
+            return False
--- src/tests.py
+++ src/tests.py
@@ -1,20 +1,43 @@
 import unittest
-import sys
-import os.path
-src = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-+ '/src/')
-sys.path.append(src)
-
 from clientes import Clientes
 
 class testClientes(unittest.TestCase):
 
+
     c = Clientes()
-    def testBusquedaPorCliente(self):
-        self.assertTrue("1", "Devuelve lo que tiene que devolver")
-        self.assertEqual(self.c.busquedaPorNombre(1), False,"Estas buscando un nombre, no puedes introducir un numero") # si introducimos un numero, la respuesta esperada seria false
-        self.assertEqual(self.c.busquedaPorNombre(" "), False,"Busqueda vacia")
-        self.assertEqual(self.c.busquedaPorNombre("maria"), False,"No existe ese cliente")
+
+    def testInicializacion(self):
+            c = Clientes()
+            self.assertIsInstance(c,Clientes);
+
+
+    def testBusquedaPorNombre(self):
+            self.assertEqual(self.c.busquedaPorNombre(1), False,"Estas buscando un nombre, no puedes introducir un numero") # si introducimos un numero, la respuesta esperada seria false
+            self.assertEqual(self.c.busquedaPorNombre(" "), None,"Busqueda vacia")
+            self.assertIsNone(self.c.busquedaPorNombre("maria"),"No existe ese cliente")
+        #    self.assertIsNone(self.c.busquedaPorNombre("Juan"),"No existe ese cliente") #Fallaria al existir
+
+
+    def testBusquedaPorDNI(self):
+            self.assertEqual(self.c.busquedaPorDNI(" "), None,"Busqueda vacia")
+            self.assertIsNone(self.c.busquedaPorDNI("66666666A"),"No hay ningun cliente con ese DNI")
+            #self.assertIsNone(self.c.busquedaPorDNI("66666666B"),"No hay ningun cliente con ese DNI")
+
+    def testBusquedaPorProvincia(self):
+            self.assertEqual(self.c.busquedaPorProvincia(1), False,"Estas buscando una provincia, no puedes introducir un numero")
+            self.assertEqual(self.c.busquedaPorProvincia(" "), None,"Busqueda vacia")
+            self.assertIsNone(self.c.busquedaPorProvincia("Malaga"),"No se encuentra esa provincia")
+
+    def testBusquedaPorEstado(self):
+            self.assertEqual(self.c.busquedaPorEstado(1), False,"Estas buscando un estado(baja/alta), no puedes introducir un numero")
+            self.assertEqual(self.c.busquedaPorEstado(" "), None,"Busqueda vacia")
+            self.assertIsNone(self.c.busquedaPorEstado("anulado"),"Solo pueden estar de alta o baja")
+
+
+    def testRobinson(self):
+        self.assertEqual(self.c.busquedaPorRobinson("Patricia"),"No puede ofrecer ofertas a este cliente")
+        #self.assertEqual(self.c.busquedaPorRobinson("Alejandro"),"No puede ofrecer ofertas a este cliente")
+
 
 if __name__ == '__main__':
     unittest.main()
